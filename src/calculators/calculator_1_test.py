2ec770faf94cc3aee4e5046bc28ff3d547901f73
Wallace Goulart <wallacegc@hotmail.com>
2026-06-08 22:24:18 -0300

    finalizing the unit tests

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from pytest import raises
 from .calculator_1 import Calculator1
 
 class MockRequest:
@@ -23,3 +24,12 @@ def test_calculate():
 
     assert response["data"]["result"] == 14.25
     assert response["data"]["Calculator"] == 1   
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "body mal formatado!"    
