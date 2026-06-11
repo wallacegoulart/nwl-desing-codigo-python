70622939c4e49db89fa05c5c9db9bea43ec691e8
Wallace Goulart <wallacegc@hotmail.com>
2026-06-11 07:40:52 -0300

    Creating factories to isolate calculators

--- src/main/routes/caulculators.py
+++ src/main/routes/caulculators.py
@@ -1,21 +1,19 @@
 from flask import Blueprint, jsonify, request
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2    
-from src.drivers.numpy_handler import NumpyHandler
+from src.main.factories.calculator1_factory import calculator1_factory
+from src.main.factories.calculator2_factory import calculator2_factory
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    calc = Calculator1()
+    calc = calculator1_factory()
     response = calc.calculate(request)
 
     return jsonify(response),200
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    numpy_handler = NumpyHandler()
-    calc = Calculator2(numpy_handler)
+    calc = calculator2_factory()
     response = calc.calculate(request)
 
     return jsonify(response),200
