bd01a5a89930adece68da3186c28cdef25d6df7b
Wallace Goulart <wallacegc@hotmail.com>
2026-06-11 07:27:43 -0300

    Uploanding a new route to calculator 2

--- src/main/routes/caulculators.py
+++ src/main/routes/caulculators.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, jsonify, request
 from src.calculators.calculator_1 import Calculator1
-from typing import Dict 
+from src.calculators.calculator_2 import Calculator2    
+from src.drivers.numpy_handler import NumpyHandler
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     calc = Calculator1()
+    response = calc.calculate(request)
+
+    return jsonify(response),200
+
+@calc_route_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    numpy_handler = NumpyHandler()
+    calc = Calculator2(numpy_handler)
     response = calc.calculate(request)
 
     return jsonify(response),200
