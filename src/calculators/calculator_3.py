4f054cf2ec68d89b6036193de30c652dea1baa1a
Wallace Goulart <wallacegc@hotmail.com>
2026-06-18 06:54:51 -0300

    finalizing the personalized tests

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -1,7 +1,8 @@
 from typing import  Dict , List
 from flask import request as FlaskRequest
 from src.drivers.interfaces.driver_handler_interface import DriverHandlerInterface
-
+from src.errors.http_unprocessable_entity import HttpUnprocessableEntityError
+from src.errors.http_bad_request import HttpBadRequestError
 class Calculator3():
     def __init__(self, driver_handler : DriverHandlerInterface ) -> None:
         self.__driver_handler = driver_handler
@@ -20,7 +21,7 @@ class Calculator3():
 
     def __validate_body(self,  body: Dict) -> List[float]:
         if "numbers" not in body:
-            raise Exception("body mal formatado!")
+            raise HttpUnprocessableEntityError("body mal formatado!")
 
         input_data = body["numbers"]
         return input_data
@@ -36,7 +37,7 @@ class Calculator3():
     
     def __verify__results(self, variance: float , multiplication: float) -> None:
         if variance < multiplication:
-            raise Exception('Falha no processo: Variância menor que multiplicação')
+            raise HttpBadRequestError('Falha no processo: Variância menor que multiplicação')
 
     def __format_response(self, variance: float) -> Dict:
         return {
